ab1b78feff9b85776a08f3ca58330d14415c282a
rayalva407 <rayalva407@gmail.com>
2024-04-08 08:12:58 -0600

    completes the tic tac toe class

---
+++ tic_tac_toe.rb
@@ -0,0 +1,58 @@
+class TicTacToe
+  def initialize
+    @board = Array.new(3) { Array.new(3, " ") }
+    @current_player = "X"
+  end
+
+  def print_board
+    @board.each { |row| puts row.map { |e| e }.join(" | ") }
+  end
+
+  def play
+    loop do
+      print_board
+      puts "Player #{@current_player}, enter your move (row [0-2], column [0-2]):"
+      row, col = gets.chomp.split.map(&:to_i)
+      if @board[row][col] == " "
+        @board[row][col] = @current_player
+        if winner?
+          print_board
+          puts "Player #{@current_player} wins!"
+          break
+        end
+        if draw?
+          print_board
+          puts "It's a draw!"
+          break
+        end
+        switch_player
+      else
+        puts "Invalid move, try again."
+      end
+    end
+  end
+
+  def winner?
+    winning_combinations = [
+      [[0, 0], [0, 1], [0, 2]], # top row
+      [[1, 0], [1, 1], [1, 2]], # middle row
+      [[2, 0], [2, 1], [2, 2]], # bottom row
+      [[0, 0], [1, 0], [2, 0]], # left column
+      [[0, 1], [1, 1], [2, 1]], # middle column
+      [[0, 2], [1, 2], [2, 2]], # right column
+      [[0, 0], [1, 1], [2, 2]], # diagonal top-left to bottom-right
+      [[0, 2], [1, 1], [2, 0]]  # diagonal top-right to bottom-left
+    ]
+    winning_combinations.any? do |positions|
+      positions.all? { |row, col| @board[row][col] == @current_player }
+    end
+  end
+
+  def draw?
+    @board.all? { |row| row.none? { |e| e == " " } }
+  end
+
+  def switch_player
+    @current_player = @current_player == "X" ? "O" : "X"
+  end
+end
