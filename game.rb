1868322bbec8bf0207a2a319cbef0d0b15a3ee9e
rayalva407 <rayalva407@gmail.com>
2024-04-08 08:34:58 -0600

    adds the game.rb file to play the game

---
+++ game.rb
@@ -0,0 +1,3 @@
+require './tic_tac_toe.rb'
+
+TicTacToe.new.play
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,54 +1,23 @@
- class TicTacToe
-    def initialize
-      @board = Array.new(3) { Array.new(3, " ") }
-      @player1 = Player.new("Player 1", "X")
-      @player2 = Player.new("Player 2", "O")
-      @current_player = @player1
-    end
+require './player.rb'
 
-    def play
-      loop do
-        print_board
-        puts "#{@current_player.name}, enter your move (row [0-2], column [0-2]):"
-        row, col = gets.chomp.split.map(&:to_i)
-        if @board[row][col] == " "
-          @board[row][col] = @current_player.symbol
-          if winner?
-            print_board
-            puts "#{@current_player.name} wins!"
-            break
-          end
-          if draw?
-            print_board
-            puts "It's a draw!"
-            break
-          end
-          switch_player
-        else
-          puts "Invalid move, try again."
-        end
-      end
-    end
-
-    def switch_player
-      @current_player = @current_player == @player1 ? @player2 : @player1
-    end
-  end
-
-  def print_board
-    @board.each { |row| puts row.map { |e| e }.join(" | ") }
+class TicTacToe
+  def initialize
+    @board = Array.new(3) { Array.new(3, " ") }
+    @player1 = Player.new("Player 1", "X")
+    @player2 = Player.new("Player 2", "O")
+    @current_player = @player1
   end
 
   def play
     loop do
       print_board
-      puts "Player #{@current_player}, enter your move (row [0-2], column [0-2]):"
+      puts "#{@current_player.name}, enter your move (row [0-2], column [0-2]):"
       row, col = gets.chomp.split.map(&:to_i)
       if @board[row][col] == " "
-        @board[row][col] = @current_player
+        @board[row][col] = @current_player.symbol
         if winner?
           print_board
-          puts "Player #{@current_player} wins!"
+          puts "#{@current_player.name} wins!"
           break
         end
         if draw?
@@ -63,6 +32,10 @@
     end
   end
 
+  def print_board
+    @board.each { |row| puts row.map { |e| e }.join(" | ") }
+  end
+
   def winner?
     winning_combinations = [
       [[0, 0], [0, 1], [0, 2]], # top row
@@ -75,7 +48,7 @@
       [[0, 2], [1, 1], [2, 0]]  # diagonal top-right to bottom-left
     ]
     winning_combinations.any? do |positions|
-      positions.all? { |row, col| @board[row][col] == @current_player }
+      positions.all? { |row, col| @board[row][col] == @current_player.symbol }
     end
   end
 
@@ -84,5 +57,6 @@
   end
 
   def switch_player
-    @current_player = @current_player == "X" ? "O" : "X"
+    @current_player = @current_player == @player1 ? @player2 : @player1
   end
+end
